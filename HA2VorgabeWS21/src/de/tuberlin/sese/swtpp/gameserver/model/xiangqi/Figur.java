70443fa4c7cabc3c69bc7f7c0b32adcc11f5d8dc
Denis Koshelev <truemacintoshchannel@gmail.com>
2022-02-17 14:19:27 +0100

    Merge

---
+++ HA2VorgabeWS21/src/de/tuberlin/sese/swtpp/gameserver/model/xiangqi/Figur.java
@@ -0,0 +1,13 @@
+package de.tuberlin.sese.swtpp.gameserver.model.xiangqi;
+
+import java.util.ArrayList;
+
+import de.tuberlin.sese.swtpp.gameserver.model.Move;
+import de.tuberlin.sese.swtpp.gameserver.model.Player;
+
+public interface Figur {
+	
+	public ArrayList<Move> getPossibleMoves(Board board, Player player);
+	
+	public Position getPosition();
+}
