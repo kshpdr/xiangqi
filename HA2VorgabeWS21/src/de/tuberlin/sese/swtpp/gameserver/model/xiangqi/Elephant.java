ee7a4679a34d4cbe16caa5e93d65981aa92a884c
Paulin  Klawon <paulin@eduroam-141-23-169-73.wlan.tu-berlin.de>
2022-02-22 14:20:58 +0100

    merge paulin2

--- HA2VorgabeWS21/src/de/tuberlin/sese/swtpp/gameserver/model/xiangqi/Elephant.java
+++ HA2VorgabeWS21/src/de/tuberlin/sese/swtpp/gameserver/model/xiangqi/Elephant.java
@@ -39,24 +39,19 @@ public class Elephant implements Figur,Serializable {
 		 * (can not jump):
 		 */
 		
-		int[][] checkArray = {{row+1,col+1},{row-1,col-1},{row+1,col-1},{row-1,col+1}};
+	
 		int[][] movesArray = {{row+2,col+2},{row-2,col-2},{row+2,col-2},{row-2,col+2}};
 		
 		// iterates over possible positions in movesArray:
 		for(int i = 0; i < 4; i++) {
 			
-			// new position:
-			Position newPos = new Position(movesArray[i][0],movesArray[i][1]);
-			// move to new position:
-			Move newMove = new Move(position.moveString(newPos), board.getBoardState(), player);
-			
 			// checks whether position is on board:
-			if(newPos.onBoard()) {
+			if(new Position(movesArray[i][0],movesArray[i][1]).onBoard()) {
 				
 				// checks whether river is being crossed:
 				if((row <= 4 && movesArray[i][0] <= 4) || (row > 4 && movesArray[i][0] > 4)) {
-					// checks whether Elephant tries to jump, myGeneral is not threatened or in-check, position is free or occupied by other player: 
-					possibleMoves.addAll(checkMove(board, newMove, newPos, player, i, checkArray, movesArray));
+					
+					possibleMoves.addAll(checkMove(board, player, movesArray, i));
 				}
 			}
 		}
@@ -67,41 +62,35 @@ public class Elephant implements Figur,Serializable {
 	
 	
 	// --> checks whether Elephant tries to jump, myGeneral is not threatened or in-check:
-	public ArrayList<Move> checkMove(Board board, Move newMove, Position newPos, Player player, int i, int[][] checkArray, int[][] movesArray) {
+	public ArrayList<Move> checkMove(Board board, Player player, int[][] movesArray, int i) {
 		
+		// current row/column:
+		int row = position.getRow();
+		int col = position.getColumn();
+				
 		ArrayList<Move> possibleMoves = new ArrayList<Move>();
 		
-		General myGeneral = board.getFriendGeneral(position);
+		int[][] checkArray = {{row+1,col+1},{row-1,col-1},{row+1,col-1},{row-1,col+1}};
 		
-		
+		// new position:
+		Position newPos = new Position(movesArray[i][0],movesArray[i][1]);
+		// move to new position:
+		Move newMove = new Move(position.moveString(newPos), board.getBoardState(), player);
+				
 		// Elephant can't jump:
 		if(board.getBoardMatrix()[checkArray[i][0]][checkArray[i][1]] == '0') {
 	
-			// checks whether myGeneral is not threatened or in-check:
-			if(!myGeneral.isThreatened(board, newMove) && !myGeneral.isCheck(board, newMove, player)) {
-				
-				possibleMoves.addAll(checkOccupiedOrFree(board, movesArray, newPos, newMove, i));
+			// checks whether position is free or occupied by other player: 
+			if(board.getBoardMatrix()[movesArray[i][0]][movesArray[i][1]] == '0') {
+				possibleMoves.add(newMove);
 			}
+			else if(position.otherPlayerOnTargetField(board, newPos)) {
+				possibleMoves.add(newMove);	
+			}
+			
+			return possibleMoves;
 		}
 		
 		return possibleMoves;
 	}
-	
-	
-	
-	// --> checks whether position is free or occupied by other player: 
-	public ArrayList<Move> checkOccupiedOrFree(Board board, int[][] movesArray, Position newPos, Move newMove, int i) {
-		
-		ArrayList<Move> possibleMoves = new ArrayList<Move>();
-		
-		if(board.getBoardMatrix()[movesArray[i][0]][movesArray[i][1]] == '0') {
-			possibleMoves.add(newMove);
-		}
-		else if(position.otherPlayerOnTargetField(board, newPos)) {
-			possibleMoves.add(newMove);	
-		}
-		
-		return possibleMoves;
-	}
-
 }
