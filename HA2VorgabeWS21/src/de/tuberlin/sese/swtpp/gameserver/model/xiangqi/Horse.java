0ea52766ba7e1327e5755b47fc95f70fdb8e6866
Paulin  Klawon <paulin@Paulins-MacBook-Pro.local>
2022-02-23 15:42:29 +0100

    horse nestedLoops update

--- HA2VorgabeWS21/src/de/tuberlin/sese/swtpp/gameserver/model/xiangqi/Horse.java
+++ HA2VorgabeWS21/src/de/tuberlin/sese/swtpp/gameserver/model/xiangqi/Horse.java
@@ -34,14 +34,12 @@ public class Horse implements Figur,Serializable {
 		
 		ArrayList<Move> possibleMoves = new ArrayList<Move>();
 		
-		General myGeneral = board.getFriendGeneral(position);
-		
 		/*
 		 * Horse moves like knight in chess (one step up/down/left/right and then one step diagonal):
 		 */
 		
+		// horse can't jump:
 		int[][] checkArray = {{row+1,col},{row,col+1},{row-1,col},{row,col-1}};
-		int[][] movesArray = {{row+2,col-1},{row+2,col+1},{row+1,col+2},{row-1,col+2},{row-2,col+1},{row-2,col-1},{row-1,col-2},{row+1,col-2}};
 
 		// iterates over checkArray:
 		for(int i = 0; i < 4; i++) {
@@ -49,31 +47,52 @@ public class Horse implements Figur,Serializable {
 			// checks whether position is on board:
 			if(new Position(checkArray[i][0], checkArray[i][1]).onBoard()) {
 				
-				// checks whether position is free (horse can't jump):
+				// checks whether horse tries to jump:
 				if(board.getBoardMatrix()[checkArray[i][0]][checkArray[i][1]] == '0') {
 					
-					// checks possible moves for each direction:		
-					for(int j = 0; j < 2; j++) {
-						
-						// new position:
-						Position newPos = new Position(movesArray[2*i+j][0],movesArray[2*i+j][1]);
-						
-						// checks whether position is on board:
-						if(newPos.onBoard()) {
-							
-							// checks whether field is free or occupied by other player (and !isThreatende and !isCheck):
-							if(board.getBoardMatrix()[movesArray[2*i+j][0]][movesArray[2*i+j][1]] == '0' && !myGeneral.isThreatened(board, new Move(position.moveString(newPos), board.getBoardState(), player)) && !myGeneral.isCheck(board, new Move(position.moveString(newPos), board.getBoardState(), player))) {
-								possibleMoves.add(new Move(position.moveString(newPos), board.getBoardState(), player));
-							}
-							else if(position.otherPlayerOnTargetField(board, newPos) && !myGeneral.isThreatened(board, new Move(position.moveString(newPos), board.getBoardState(), player)) && !myGeneral.isCheck(board, new Move(position.moveString(newPos), board.getBoardState(), player))) {
-								possibleMoves.add(new Move(position.moveString(newPos), board.getBoardState(), player));	
-							}	
-						}
-					}	
+					possibleMoves.addAll(moveCheckHorse(board, player, i));
 				}
 			}
 		}
 		
 		return possibleMoves;
 	}
+	
+	// extends getPossibleMoves:
+	public ArrayList<Move> moveCheckHorse(Board board, Player player, int i) {
+		
+		// current row/column:
+		int row = position.getRow();
+		int col = position.getColumn();
+		
+		ArrayList<Move> possibleMoves = new ArrayList<Move>();
+	
+		// possible moves:
+		int[][] movesArray = {{row+2,col-1},{row+2,col+1},{row+1,col+2},{row-1,col+2},{row-2,col+1},{row-2,col-1},{row-1,col-2},{row+1,col-2}};
+		
+		// checks possible moves for each direction:		
+		for(int j = 0; j < 2; j++) {
+			
+			// new position:
+			Position newPos = new Position(movesArray[2*i+j][0],movesArray[2*i+j][1]);
+			// new move:
+			Move newMove = new Move(position.moveString(newPos), board.getBoardState(), player);
+			
+			// checks whether position is on board:
+			if(newPos.onBoard()) {
+				
+				// checks whether field is free or occupied by other player:
+				if(board.getBoardMatrix()[movesArray[2*i+j][0]][movesArray[2*i+j][1]] == '0') {
+					possibleMoves.add(newMove);
+				}
+				else if(position.otherPlayerOnTargetField(board, newPos)) {
+					possibleMoves.add(newMove);	
+				}	
+			}
+		}
+		
+		return possibleMoves;
+		
+	}
+	
 }
